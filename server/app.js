8e05b4770212365f01f7282015d50a203e25ec58
mizgaionutalexandru <mizgaionutalexandru@gmail.com>
2022-08-14 12:11:45 +0300

    added initial express config

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const { PORT } = require('./config');
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
