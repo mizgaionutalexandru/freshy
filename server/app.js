5a264affab5e09b6c09a7a3bffa253ceb6c52e42
mizgaionutalexandru <mizgaionutalexandru@gmail.com>
2022-08-14 14:52:11 +0300

    added env config and db connection

--- server/app.js
+++ server/app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
-const { PORT } = require('./config');
+const mongoose = require('mongoose');
+require('dotenv').config({
+  path: './config.env',
+});
 
 const itemRouter = require('./routes/itemsRoutes');
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
-});
+// Connecting to DB
+mongoose
+  .connect(process.env.DB.replace('<password>', process.env.DB_PASS))
+  .then(() => {
+    console.log('Connection to DB has been established! ✅');
+  })
+  .catch((err) => {
+    console.log('Connection to DB has failed ❌❌❌');
+    console.log(err);
+  });
 
 // API routes
 app.use('/api/v1/items', itemRouter);
@@ -14,6 +24,6 @@ app.use('/api/v1/items', itemRouter);
 // TODO: add error handler
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`App running on port ${process.env.PORT} ✅`);
 });
