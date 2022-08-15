950e5062fed2874c1dbf03f0c00b155db4da5415
mizgaionutalexandru <mizgaionutalexandru@gmail.com>
2022-08-15 12:20:57 +0300

    added catchAsync refacturing

--- server/controllers/itemsController.js
+++ server/controllers/itemsController.js
@@ -1,6 +1,7 @@
 const Item = require('./../models/itemsModel');
+const catchAsync = require('./../utils/catchAsync');
 
-module.exports.getAllItems = async (req, res) => {
+module.exports.getAllItems = catchAsync(async (req, res) => {
   const items = await Item.find({});
 
   res.status(200).json({
@@ -10,26 +11,22 @@ module.exports.getAllItems = async (req, res) => {
       items,
     },
   });
-};
+});
 
 module.exports.getItem = (req, res) => {
   res.send(`Getting the item ${req.params.id}...`);
 };
 
-module.exports.createItem = async (req, res) => {
-  try {
-    const item = await Item.create(req.body);
+module.exports.createItem = catchAsync(async (req, res) => {
+  const item = await Item.create(req.body);
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        data: item,
-      },
-    });
-  } catch (err) {
-    console.log(`❌❌ ${err}`);
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: item,
+    },
+  });
+});
 
 module.exports.updateItem = (req, res) => {
   res.send(`The item ${req.params.id} is being UPDATED...`);
